5539525c1781d60115999189ebffbcea4186ddb3
Lova Duvnäs <lova.duvnas@yh.nackademin.se>
2020-09-28 17:52:48 +0200

    Add FlexContainer, LinkContainer and Navbar to DetailPage

--- src/pages/CustomerDetailPage.jsx
+++ src/pages/CustomerDetailPage.jsx
@@ -4,12 +4,24 @@ import { useHistory, Link } from "react-router-dom";
 import Button from "../components/Button";
 import styled from "styled-components";
 import { AiOutlineArrowLeft, AiOutlineDelete } from "react-icons/ai";
+import Navbar from "../components/Navbar";
 
 const CustomerDetailGrid = styled.div`
   display: grid;
   grid-template-columns: repeat(4, 1fr);
   grid-template-rows: auto;
   grid-gap: 1em;
+`;
+
+const LinkContainer = styled.a`
+  display: flex;
+  align-items: center;
+`;
+
+const FlexContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
 `;
 
 export default function CustomerDetailPage(props) {
@@ -41,42 +53,47 @@ export default function CustomerDetailPage(props) {
   }
 
   return (
-    <div className="container">
-      <Link to={`/home`}>
-        <AiOutlineArrowLeft />
-        Back to home page
-      </Link>
+    <FlexContainer>
+      <Navbar />
+      <div className="container">
+        <Link to={`/home`}>
+          <LinkContainer>
+            <AiOutlineArrowLeft />
+            Back to home page
+          </LinkContainer>
+        </Link>
 
-      <h2>{customerData.name}</h2>
-      <CustomerDetailGrid>
-        <div>
-          <h6>Name:</h6> <p> {customerData.name}</p>
-        </div>
+        <h2>{customerData.name}</h2>
+        <CustomerDetailGrid>
+          <div>
+            <h6>Name:</h6> <p> {customerData.name}</p>
+          </div>
 
-        <div>
-          <h6>organistationNr:</h6> <p> {customerData.organisationNr}</p>
-        </div>
-        <div>
-          <h6>vatNr:</h6> <p>{customerData.vatNr}</p>
-        </div>
-        <div>
-          <h6>Reference:</h6> <p> {customerData.reference}</p>
-        </div>
-        <div>
-          <h6>PaymentTerm:</h6> <p>{customerData.paymentTerm}</p>
-        </div>
-        <div>
-          <h6>Website:</h6> <p>{customerData.website}</p>
-        </div>
-        <div>
-          <h6>Email: </h6> <p>{customerData.email}</p>
-        </div>
-        <div>
-          <h6>PhoneNumber:</h6> <p>{customerData.phoneNumber}</p>
-        </div>
-      </CustomerDetailGrid>
-      <AiOutlineDelete />
-      <Button onClick={deleteCustomer} title={"Delete customer"} />
-    </div>
+          <div>
+            <h6>organistationNr:</h6> <p> {customerData.organisationNr}</p>
+          </div>
+          <div>
+            <h6>vatNr:</h6> <p>{customerData.vatNr}</p>
+          </div>
+          <div>
+            <h6>Reference:</h6> <p> {customerData.reference}</p>
+          </div>
+          <div>
+            <h6>PaymentTerm:</h6> <p>{customerData.paymentTerm}</p>
+          </div>
+          <div>
+            <h6>Website:</h6> <p>{customerData.website}</p>
+          </div>
+          <div>
+            <h6>Email: </h6> <p>{customerData.email}</p>
+          </div>
+          <div>
+            <h6>PhoneNumber:</h6> <p>{customerData.phoneNumber}</p>
+          </div>
+        </CustomerDetailGrid>
+        <AiOutlineDelete />
+        <Button onClick={deleteCustomer} title={"Delete customer"} />
+      </div>
+    </FlexContainer>
   );
 }
