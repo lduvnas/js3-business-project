f2d92a592f835be1169ca4faacaf5e1e35c430ff
Lova Duvnäs <lova.duvnas@yh.nackademin.se>
2020-09-28 20:20:22 +0200

    Create styles components Button, Container, FlexContainer

---
+++ src/styles/Button.js
@@ -0,0 +1,16 @@
+import styled from "styled-components";
+
+const Button = styled.button`
+  width: 200px;
+  padding: 12px 20px;
+  margin: 8px 0;
+  border-radius: 5px;
+  border: none;
+  background-color: #67a4d9;
+  color: white;
+  &:hover {
+    background-color: #5885ac;
+  }
+`;
+
+export default Button;
---
+++ src/styles/Container.js
@@ -0,0 +1,12 @@
+import styled from "styled-components";
+
+const Container = styled.div`
+  width: 800px;
+  background-color: white;
+  padding: 3rem;
+  box-shadow: 0 1px 15px rgba(113, 113, 113, 0.06),
+    0 5px 12px rgba(211, 211, 211, 0.08);
+  border-radius: 5px;
+`;
+
+export default Container;
