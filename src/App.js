b5a3ef1020f386ea9eaaf96128efbfb1c98e3d46
Lova Duvnäs <lova.duvnas@yh.nackademin.se>
2020-09-28 17:58:24 +0200

    Removed unused code in App.js

--- src/App.js
+++ src/App.js
@@ -3,26 +3,18 @@ import { Switch, Route } from "react-router-dom";
 import LoginPage from "./pages/LoginPage";
 import HomePage from "./pages/HomePage";
 import RegisterPage from "./pages/RegisterPage";
-import styled from "styled-components";
 import ConfirmPage from "./pages/ConfirmPage";
 import CustomerDetailPage from "./pages/CustomerDetailPage";
 import { CustomerListContext } from "./contexts/CustomerListContext";
 import { GlobalStyles } from "./styles/GlobalStyles";
-import { DotScale } from "styled-loaders-react";
-
-const Title = styled.h1`
-  font-size: 2.5em;
-  text-align: center;
-`;
 
 function App() {
   const [customerList, setCustomerList] = useState(null);
 
   return (
-    <div className="App">
-      <DotScale color="#ccc" />
+    <div>
       <GlobalStyles></GlobalStyles>
-      <Title>Business Project</Title>
+
       <CustomerListContext.Provider value={{ customerList, setCustomerList }}>
         <Switch>
           <Route path="/customers/:id" component={CustomerDetailPage} />
